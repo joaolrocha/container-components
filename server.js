eec1f8559b78340250f902c556d3ab2811aa819d
joaolrocha <joaolrocha07@gmail.com>
2023-12-20 11:51:58 -0300

    feat: creating a user loader to search user by id

--- server.js
+++ server.js
@@ -17,18 +17,21 @@ let users = [
     age: 55,
     country: "United Kingdom",
     books: ["Fingersmith", "The Night Watch"],
+    id: "2"
   },
   {
     name: "Haruki Murakami",
     age: 71,
     country: "Japan",
     books: ["Norwegian Wood", "Kafka on the Shore"],
+    id: "3"
   },
   {
     name: "Chimamanda Ngozi Adichie",
     age: 43,
     country: "Nigeria",
     books: ["Half of a Yellow Sun", "Americanah"],
+    id: "4"
   },
 ];
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import { CurrentUserLoader } from './components/current-user-loader';
 import { UserInfo } from './components/user-info';
+import { UserLoader } from './components/user-loader';
+
 
 function App() {
   return (
     <>
-      <CurrentUserLoader>
+      <UserLoader userId={"3"}>
         <UserInfo />
-      </CurrentUserLoader>
+      </UserLoader>
     </>
 
   );
